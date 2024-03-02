0bcef7e5e6b11081811f40a96aea7e96c587dd27
anton <antonirenault21@gmail.com>
2024-03-02 02:27:05 +0100

    Set-Up

---
+++ src/main/java/fr/papillonlut/butterfly/ButterflyClient.java
@@ -0,0 +1,10 @@
+package fr.papillonlut.butterfly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ButterflyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/fr/papillonlut/butterfly/ButterflyDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.papillonlut.butterfly;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ButterflyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
